3d7a3e292a53e3b858137d3519ea9631148db35e
Bogdan <bs@24h.tv>
2022-09-13 10:15:03 +0300

    fix menu

--- Djsite/coolsite48/women/utils.py
+++ Djsite/coolsite48/women/utils.py
@@ -19,7 +19,7 @@ class DataMixin:
         user_menu = deepcopy(menu)
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
-        context['menu'] = menu
+        context['menu'] = user_menu
         context['cats'] = cats
         if 'cat_selected' not in context:
             context['cat_selected'] = 0
