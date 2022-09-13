ff7c7480655396118299ad8c0d4b1761b763a47c
Bogdan <bs@24h.tv>
2022-09-13 10:16:30 +0300

    use dict.update

--- Djsite/coolsite48/women/views.py
+++ Djsite/coolsite48/women/views.py
@@ -19,7 +19,7 @@ class WomenHome(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Главная страница")
-        return dict(list(context.items()) + list(c_def.items()))
+        return context.update(c_def)
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
