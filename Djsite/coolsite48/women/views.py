eddacb39325b363ae290561660cda81606ea8a64
Bogdan <bs@24h.tv>
2022-09-13 10:48:33 +0300

    change return

--- Djsite/coolsite48/women/views.py
+++ Djsite/coolsite48/women/views.py
@@ -19,7 +19,8 @@ class WomenHome(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Главная страница")
-        return context.update(c_def)
+        context.update(c_def)
+        return context
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
